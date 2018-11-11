03ad1f11142bd97ad69a108480e6c96c60bb9251
thanhdhps05227 <44937613+thanhdhps05227@users.noreply.github.com>
2018-11-11 18:02:20 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,11 +1,10 @@
 <?php
-return [
 $url = parse_url(getenv("DATABASE_URL"));
 $host = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $database = substr($url["path"], 1);
-
+return [
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
